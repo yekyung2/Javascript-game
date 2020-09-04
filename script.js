7799345a4c7fb45e7936132918f0f0fdfe4a2791
deepdiver <time0909@g.skku.edu>
2020-09-05 00:17:29 +0900

    test

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function moveRight(){
     var left = 
     parseInt(window.getComputedStyle(character).getPropertyValue("left"));
     character.style.left = left + 2 + "px";
+    
 }
 
 document.addEventListener("keydown",event => {
